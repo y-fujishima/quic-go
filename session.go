a1879d27b40086bb6bf74edba2af551088e8f95c
Marten Seemann <martenseemann@gmail.com>
2021-01-11 14:40:37 +0800

    add support for parsing ACK_FREQUENCY frames to the frame parser

--- session.go
+++ session.go
@@ -478,7 +478,7 @@ var newClientSession = func(
 func (s *session) preSetup() {
 	s.sendQueue = newSendQueue(s.conn)
 	s.retransmissionQueue = newRetransmissionQueue(s.version)
-	s.frameParser = wire.NewFrameParser(s.config.EnableDatagrams, s.version)
+	s.frameParser = wire.NewFrameParser(s.config.EnableDatagrams, false, s.version)
 	s.rttStats = &utils.RTTStats{}
 	s.connFlowController = flowcontrol.NewConnectionFlowController(
 		protocol.InitialMaxData,
